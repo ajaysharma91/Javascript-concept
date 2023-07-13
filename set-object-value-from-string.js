68b75588cb71341987255059ca91d67e576252a1
ajaysharma91 <ajparashar111@gmail.com>
2023-07-13 23:52:03 +0530

    Added new set value in object

---
+++ set-object-value-from-string.js
@@ -0,0 +1,31 @@
+const helper = (obj,path,value)=>{
+    const [current,...rest] = path;
+    if(rest.length>0){
+        if(!obj[current]){
+            const isNumeric = !isNaN(rest[0]) && rest[0] !== null;
+            console.log({isNumeric})
+            obj[current] = isNumeric?[]:{};
+        }
+        if(typeof obj[current] == 'object'){
+            const isNumeric = !isNaN(rest[0]) && rest[0] !== null;
+            obj[current] = helper(isNumeric?[]:{},rest,value)
+        }else{
+            obj[current] = helper(obj[current],rest,value)
+        }
+    }
+    else{
+        obj[current] = value;
+    }
+    return obj;
+}
+
+const setValue = (obj,path,value)=>{
+    const pathMate = path.replaceAll('[','.').replaceAll(']','').split('.');
+    helper(obj,pathMate,value)
+    return obj;
+}
+
+
+
+console.log(setValue({},'a[0].b.c',5))
+console.log(setValue({},[x,0,s,d],3))
